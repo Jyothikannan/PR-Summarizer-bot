9c334ce77795cf71673614589db5463db3eb1da8
Jyothikannan <jyothikannan218@gmail.com>
2025-11-30 12:55:06 +0530

    Fix URLs and add homepage

--- prbot/core/urls.py
+++ prbot/core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import analyze_pr
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the PR Summarizer Bot!")
 
 urlpatterns = [
-    path("analyze/", analyze_pr, name="analyze_pr"),
+    path('', home, name='home'),         # root URL
+    path('analyze/', analyze_pr, name='analyze_pr'),
 ]
